60d9399c57e967bdb011080ee4e5f565826d1d85
Art Santana <Art Santana>
2017-09-07 18:24:58 -0400

    newest

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ use burgers_db;
 
 create table burgers (
 id INTEGER(30) AUTO_INCREMENT NOT NULL,
-burger_name VARCHAR(50),
-devoured BOOLEAN,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN DEFAULT false,
 date timestamp,
 PRIMARY KEY(id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+/* insert into database INSERT INTO *table name* (where, what) VALUES ("name of burger", FALSE);*/
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
+
+
