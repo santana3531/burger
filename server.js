cb1cd95a360be14b3c9adc2953e5f8a625d8437f
Art Santana <Art Santana>
2017-09-14 10:22:50 -0400

    burger server.js

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
 var express = require(express);
-var methodoverride = require(method-override);
-var bodyparser = require(body-parser);
+var bodyParser = require(body-parser);
+var methodOverride = require(method-override);
+var app = express();
+var PORT = process.env.PORT || 3000;
 
+
+app.use(express.static(__dirname + "/public"));
+
+// make the app parse
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// use the override with post
+app.use(methodOverride("_method"));
+
+// require the handlebars
+var exphbs = require("express-handlebars");
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main "}));
+app.set("view engine", "handlebars");
+
+
+var routes = require("./controllers/burgersController.js")
+
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+
+app.listen(PORT, function() {
+	console.log("listening on port: %s", PORT);
+});
